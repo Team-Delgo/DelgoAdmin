ce0f5dd3465d784311d7e4500bd6dc99bf8b41de
ckrb63 <cksr1@naver.com>
2023-04-19 13:20:51 +0900

    INIT : PROJECT

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // 1. react-router-dom 으로 페이지 연결, path는 common/constants폴더에서 상수로 관리
+  // 2. scss(sass)로 css 하기
+  // 3. redux-toolkit으로 전역 상태관리
+  // 4. axios로 api 호출, api파일은 다른 repository보고 참조
+  // 5. app.scss에 전역 font 정의
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
